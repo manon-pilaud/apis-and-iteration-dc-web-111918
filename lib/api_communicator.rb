2ecb76f75744e289e7d66ae3bd378738259fe260
github username <github email address>
2018-11-20 14:24:36 -0500

    beau mods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,9 +4,19 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
+  films =[]
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+  response_hash["results"].each do |character_data|
+    if character_data["name"].include?(character_name)
+      character_data["films"].each do |film|
+        film_response_string = RestClient.get(film)
+        film_response_hash = JSON.parse(film_response_string)
+          films << film_response_hash
+      end
+    end
+  end
+  films
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,6 +30,7 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+
 end
 
 def show_character_movies(character)
